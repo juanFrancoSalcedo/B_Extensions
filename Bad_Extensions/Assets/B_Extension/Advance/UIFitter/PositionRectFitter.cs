63c134508ce2dc4d504795e64e1df740f7fe56ec
Sebastián Franco <regsebas@gmail.com>
2024-12-24 11:56:23 -0500

    [fix] return position to original code

--- Bad_Extensions/Assets/B_Extension/Advance/UIFitter/PositionRectFitter.cs
+++ Bad_Extensions/Assets/B_Extension/Advance/UIFitter/PositionRectFitter.cs
@@ -1,65 +1,32 @@
-
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace B_Extensions.ScreenFitter
+public class PositionRectFitter : MonoBehaviour
 {
-    public class PositionRectFitter : MonoBehaviour
+    [SerializeField] Vector3 positionHorizontal;
+    [SerializeField] Vector3 positionVertical;
+
+    RectTransform _rectTransform;
+    private void Awake()
     {
-        //[SerializeField] Vector3 positionHorizontal;
-        //[SerializeField] Vector3 positionVertical;
-        [SerializeField] List<PositionFitterSheet> positionsChecks = new List<PositionFitterSheet>();
-
-        Vector2 screenSizeBuffer = Vector2.zero;
-
-        RectTransform _rectTransform;
-        private void Awake()
-        {
-            _rectTransform = (RectTransform)transform;
-        }
-
-        private void Update()
-        {
-            if (screenSizeBuffer.x != Screen.width && screenSizeBuffer.y != Screen.height)
-            {
-                positionsChecks.ForEach(p => p.Limit(_rectTransform));
-                screenSizeBuffer = new Vector2(Screen.width, Screen.height);
-            }
-        }
+        _rectTransform = (RectTransform)transform;
     }
 
-    [System.Serializable]
-    public class PositionFitterSheet
+    private void Update()
     {
-        public bool useWidth;
-        public bool useHeight;
-        public float minWidth;
-        public float maxWidth;
-        public float minHeight;
-        public float maxHeight;
-        public Vector3 targetPosition;
-
-        public void Limit(RectTransform _rectTra)
-        {
-            //Screen.width
-            if (useWidth && IsInRange(Screen.width, minWidth, maxWidth))
-            {
-                
-                _rectTra.anchoredPosition = targetPosition;
-            }
-
-            if (useHeight && IsInRange(Screen.height, minHeight, maxHeight))
-            {
-                
-                _rectTra.anchoredPosition = targetPosition;
-            }
-
-        }
-        public bool IsInRange(float value, float min, float max)
-        {
-            return value >= min && value <= max;
-        }
+        if (Screen.width > Screen.height)
+            ApplyHorizontalPosition();
+        else
+            ApplyVerticalPosition();
     }
 
+    private void ApplyVerticalPosition()
+    {
+        _rectTransform.anchoredPosition = positionVertical;
+    }
+
+    private void ApplyHorizontalPosition()
+    {
+        _rectTransform.anchoredPosition = positionHorizontal;
+    }
 }
