6a3d4a55738aef8f4a561198aef63e2f3c5b8fb1
sebastian franco <sebastian.newrona.net@gmail.com>
2024-12-25 23:20:17 -0500

    [refactor] directiva test script eliminaton

---
+++ Anima DG/Assets/Anima DG Control/DirectivaDoTweens.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class DirectivaDoTweens
+{
+    static DirectivaDoTweens()
+    {
+        string scriptDirectiva = "Assets/Plugins/Demigiant/DOTweenPro/DOTweenPro.dll";
+        string symboDefinition = "ANIMA_DOTWEEN_PRO";
+
+        if (File.Exists(scriptDirectiva))
+        {
+            AddDefineSymbol(symboDefinition);
+            //Debug.Log("Existe");
+        }
+        else
+        {
+            //Debug.Log("No ERxiste");
+            RemoveDefine(symboDefinition);
+        }
+    }
+
+    private static void AddDefineSymbol(string definesSymbol)
+    {
+        var targetGroup = NamedBuildTarget.Standalone;
+        string defines = PlayerSettings.GetScriptingDefineSymbols(targetGroup);
+        if (!defines.Contains(definesSymbol))
+        {
+            PlayerSettings.SetScriptingDefineSymbols(targetGroup, defines + ";" + definesSymbol);
+            Debug.Log("Enter");
+        }
+    }
+
+    private static void RemoveDefine(string defineSymbol)
+    {
+        var targetGroup = NamedBuildTarget.Standalone;
+        string defines = PlayerSettings.GetScriptingDefineSymbols(targetGroup);
+        if (defines.Contains(defineSymbol))
+        {
+            PlayerSettings.SetScriptingDefineSymbols(targetGroup, defines.Replace(defineSymbol, "").Replace(";;", ";"));
+        }
+    }
+}
